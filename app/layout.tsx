fc9ec3080f8e6e35a204c89bbbe24199a6876bbd
Tristan Ko <tkyiko@uwaterloo.ca>
2025-11-10 10:37:50 -0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { StickyHeader } from "@/components/sticky-header";
 import { Footer } from "@/components/footer";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Tristan Ko - Portfolio",
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
